cb6dc3f4ba7b27e1e997c2114a22335f4dc8e653
Thiago Salome <thiagosalome@gmail.com>
2020-11-29 16:41:06 -0300

    Finalizando dark mode da pagina interna

--- pages/interna.tsx
+++ pages/interna.tsx
@@ -16,9 +16,9 @@ export default function Home() {
       </Head>
       <main className='xl:flex'>
         <Sidebar />
-        <div className='flex-grow'>
+        <div className='flex-grow dark:bg-black'>
           <Header />
-          <section className='bg-gray-100 rounded-tl-3xl h-dashboard-content overflow-y-auto xl:h-xl:dashboard-content'>
+          <section className='bg-gray-100 rounded-tl-3xl h-dashboard-content overflow-y-auto dark:bg-gray-800 xl:h-xl:dashboard-content'>
             <div id='banner' className='relative px-4 py-5 xl:px-9'>
               <img src='https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/58PON1OrnBiX6CqEHgeWKVwrCn6.jpg' className='absolute top-0 left-0 h-full w-full object-cover' alt='Fear the Walking Dead' title='Fear the Walking Dead' />
               <button className='absolute left-4 top-6 xl:left-8'><IoMdArrowBack color='#FFFFFF' size={35} /></button>
@@ -49,35 +49,35 @@ export default function Home() {
               </button>
             </div>
             <div id='sinopse' className='px-4 pb-4 xl:p-9 xl:ml-64 xl:-mt-56'>
-              <h3 className='text-2xl font-medium text-black'>Sinopse</h3>
-              <p className='text-lg text-black leading-5 mt-2 mb-6'>
+              <h3 className='text-2xl font-medium text-black dark:text-white'>Sinopse</h3>
+              <p className='text-lg text-black leading-5 mt-2 mb-6 dark:text-white'>
                 Ambientada em Los Angeles, a série mostra o começo do apocalipse zumbi e a temível desintegração da sociedade pelos olhos de uma família disfuncional, que precisa se unir para tentar sobreviver ao caos do fim dos tempos.
               </p>
               <div className='grid grid-cols-2 grid-rows-3 gap-x-9 gap-y-5 xl:grid-cols-5 xl:grid-rows-1'>
                 <div>
-                  <h4 className='text-base font-medium text-black'>Situação</h4>
-                  <p className='text-lg text-black'>Renovada</p>
+                  <h4 className='text-base font-medium text-black dark:text-white'>Situação</h4>
+                  <p className='text-lg text-black dark:text-white'>Renovada</p>
                 </div>
                 <div>
-                  <h4 className='text-base font-medium text-black'>Emissora</h4>
-                  <p className='text-lg text-black'>AMC</p>
+                  <h4 className='text-base font-medium text-black dark:text-white'>Emissora</h4>
+                  <p className='text-lg text-black dark:text-white'>AMC</p>
                 </div>
                 <div>
-                  <h4 className='text-base font-medium text-black'>Tipo</h4>
-                  <p className='text-lg text-black'>Roteirizada</p>
+                  <h4 className='text-base font-medium text-black dark:text-white'>Tipo</h4>
+                  <p className='text-lg text-black dark:text-white'>Roteirizada</p>
                 </div>
                 <div>
-                  <h4 className='text-base font-medium text-black'>Idioma Original</h4>
-                  <p className='text-lg text-black'>Inglês</p>
+                  <h4 className='text-base font-medium text-black dark:text-white'>Idioma Original</h4>
+                  <p className='text-lg text-black dark:text-white'>Inglês</p>
                 </div>
                 <div>
-                  <h4 className='text-base font-medium text-black'>Classificação</h4>
-                  <p className='text-lg text-black'>16 anos</p>
+                  <h4 className='text-base font-medium text-black dark:text-white'>Classificação</h4>
+                  <p className='text-lg text-black dark:text-white'>16 anos</p>
                 </div>
               </div>
             </div>
             <div id='imagens-de-fundo' className='p-4 xl:px-9'>
-              <h3 className='text-2xl font-medium text-black'>Imagens de Fundo</h3>
+              <h3 className='text-2xl font-medium text-black dark:text-white'>Imagens de Fundo</h3>
               <div className='grid grid-cols-background-images grid-rows-background-images gap-5 mt-6 -mx-4 px-4 overflow-x-auto xl:grid-cols-12'>
                 <figure className='overflow-hidden h-full w-full rounded-2xl xl:col-start-1 xl:col-end-4 '>
                   <img src='https://image.tmdb.org/t/p/w533_and_h300_bestv2/58PON1OrnBiX6CqEHgeWKVwrCn6.jpg' className='h-full w-full object-cover' alt=''/>
@@ -100,7 +100,7 @@ export default function Home() {
               </div>
             </div>
             <div id='os-mais-populares' className='p-4 xl:px-9'>
-              <h3 className='text-2xl font-medium text-black'>Elenco</h3>
+              <h3 className='text-2xl font-medium text-black dark:text-white'>Elenco</h3>
               <div className='grid grid-cols-card-movies grid-rows-2 gap-5 my-5 px-4 -mx-4 overflow-x-auto xl:grid-cols-5'>
                 <CardCast image='https://image.tmdb.org/t/p/w138_and_h175_face/jOGFt9WUezPQFN0xxgTekA2Fyta.jpg' name='Alycia Debnam-Carey' character='Alicia Clark' episodes='76 episódios' />
                 <CardCast image='https://image.tmdb.org/t/p/w138_and_h175_face/iS4vwXHoPhOdwUQS7J9kplitvlh.jpg' name='Danay García' character='Luciana Galvez' episodes='61 episódios' />
